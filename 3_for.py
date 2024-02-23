94ce08052040c0fbdd4acc9bd08827aa0510a70c
mks3011 <bmb3011@gmail.com>
2024-02-23 17:46:53 +0300

    №3 done

--- 3_for.py
+++ 3_for.py
@@ -6,7 +6,7 @@
 
 * Дан список словарей с данными по колличеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -26,15 +26,15 @@ def main():
         {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
         {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
     ]
-    
+
     def count_sum(itms_sld):
         ttl_sld = 0
+        avg_sld = 0
         for sold in itms_sld:
             ttl_sld += sold
             avg_sld = ttl_sld / len(itms_sld)
-        return(ttl_sld, avg_sld)
-    
-    
+        return ttl_sld, avg_sld
+
     all_items_sum = 0
     avg_items_sum = 0
 
@@ -43,13 +43,13 @@ def main():
         t, a = phone_ttl
         all_items_sum += t
         avg_items_sum += a
-    
+
         print(f'Cуммарное количество продаж модели {one_phone["product"]}: {t}')
         print(f'Среднее количество продаж модели {one_phone["product"]}: {int(a)}')
-    print(f"Суммарное количество продаж всех товаров: {all_items_sum}")    
+    print(f"Суммарное количество продаж всех товаров: {all_items_sum}")
     print(f"Среднее количество продаж всех товаров: {int(avg_items_sum)}")
 
-            
-        
+
+
 if __name__ == "__main__":
     main()

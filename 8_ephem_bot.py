d508684cc39fda5ebcac4580644a514d468c107c
mks3011 <bmb3011@gmail.com>
2024-02-24 14:25:07 +0300

    №8 done

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -13,8 +13,13 @@
 
 """
 import logging
+# import settings
+import ephem
+
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import date
+today = date.today()
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -31,24 +36,45 @@ PROXY = {
 
 
 def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+    print("Нажат /start")
+    # print(update)
+    # print(context)
+    username = update.message.chat.username
+    # print(username)
+    update.message.reply_text(f"Привет {username}")
 
 
 def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
+    text = update.message.text
+    # print(text)
     update.message.reply_text(text)
+
+
+def planets(update, context):
+    planet_name = update.message.text.split()[1]
+    print(planet_name)
+    if planet_name == 'Neptune':
+        n = ephem.constellation(ephem.Neptune(today))
+        update.message.reply_text(f'Сегодня планета {planet_name} находится в созвездии {n}')
+    elif planet_name == 'Mars':
+        m = ephem.constellation(ephem.Mars(today))
+        update.message.reply_text(f'Сегодня планета {planet_name} находится в созвездии {m}')
+    elif planet_name == 'Sun':
+        s = ephem.constellation(ephem.Sun(today))
+        update.message.reply_text(f'Сегодня планета {planet_name} находится в созвездии {s}')
+    else:
+        update.message.reply_text(f'Ушел искать планету {planet_name}')
 
 
 def main():
     mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planets))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
+    logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 
